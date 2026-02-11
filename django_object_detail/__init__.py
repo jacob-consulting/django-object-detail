9bef3bfc63a8c1321c766308cdd91e1746f5125d
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2026-02-11 10:15:06 +0100

    Bump version: 0.1.6 → 0.1.7

--- django_object_detail/__init__.py
+++ django_object_detail/__init__.py
@@ -1,4 +1,4 @@
 from django_object_detail.config import BadgeConfig, LinkConfig, PropertyConfig, PropertyGroupConfig, x
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __all__ = ["BadgeConfig", "LinkConfig", "PropertyConfig", "PropertyGroupConfig", "x"]
